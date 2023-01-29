75a111d2529155bc0a62173ff08bde02ddea56f3
datastsea <datastsea@gmail.com>
2023-01-29 22:22:09 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Instagram Follow Check
-인스타그램 팔로우 여부 확인
+- 인스타그램 팔로우 여부 확인
+- 프로그램을 실행하려는 컴퓨터에 크롬 브라우저가 설치되어 있어야 함 (프로그램 실행 중에 크롬 브라우저 동작)
+- Webdriver Manager로 설치된 드라이버는 `~/.wdm` 위치에 설치됨
+- 팔로우 리스트를 가져오는 동안 타임슬립 존재
+
+## Update Date
+2023-01-29 (미완)
+
+## Language & Library
+- Python 3.9.16
+- Selenium 4.8.0 (`pip install selenium`)
+- Webdriver Manager 3.8.5 (`pip install webdriver_manager`)
+- BeautifulSoup 4.11.1 (`pip install bs4`)
+```bash
+$ python --version     
+Python 3.9.16
+
+$ pip list
+Package           Version
+----------------- --------
+selenium          4.8.0
+webdriver-manager 3.8.5
+beautifulsoup4    4.11.1
+...
+```
+
+## Reference
+- 크롬 드라이버: https://chromedriver.chromium.org/downloads
---
+++ follow.py
@@ -0,0 +1,78 @@
+import sys
+import time
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.webdriver import WebDriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+TIME_WAIT = 60
+TIME_SLEEP = 3
+INSTAGRAM_URL = 'https://www.instagram.com'
+
+
+def get_follow_list(driver: WebDriver, id: str, tag: str) -> list:
+    """
+    태그(followers, following)에 맞는 팔로우 리스트를 가져온다.
+        Args:
+            driver (WebDriver): 크롬 브라우저 드라이버
+            id (str): 아이디 값
+            tag (str): 태그 값 (followers, following)
+        Returns:
+            팔로우 list 반환
+    """
+    # Move to Follow Page
+    driver.get(INSTAGRAM_URL + f'/{id}/{tag}/')
+    time.sleep(TIME_SLEEP)
+    
+    # Scroll Down to Get Follow List
+    old_height, new_height = 0, 1
+    dialog_element = "document.getElementsByClassName('_aano')[0]"
+    while old_height != new_height:
+        old_height = driver.execute_script(f'return {dialog_element}.scrollHeight')
+        driver.execute_script(f'{dialog_element}.scrollTo(0, {dialog_element}.scrollHeight)')
+        time.sleep(TIME_SLEEP)
+        new_height = driver.execute_script(f'return {dialog_element}.scrollHeight')
+    
+    # Parsing
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    follow_list = [follow.get_text() for follow in soup.find_all('a', ['notranslate'])]
+
+    print(f'{tag}:', follow_list)
+    print(f'total count: {len(follow_list)}\n')
+    return follow_list
+
+
+def main(driver: WebDriver, id: str, pw: str):
+    ## Login
+    # TODO: 로그인 실패시 프로그램 종료
+    driver.get(INSTAGRAM_URL)
+    driver.find_element(by=By.NAME, value='username').send_keys(id)
+    driver.find_element(by=By.NAME, value='password').send_keys(pw)
+    driver.find_element(by=By.XPATH, value='//*[@id="loginForm"]/div[1]/div[3]/button').submit()
+    driver.find_element(by=By.XPATH, value='//main/div/div/div/div/button').click() # Click Later Button
+    
+    ## Get List
+    followers_list = get_follow_list(driver, id, 'followers')
+    following_list = get_follow_list(driver, id, 'following')
+
+    ## Calculate Unfollowers
+    unfollowers_list = list(set(following_list) - set(followers_list))
+    print('unfollowers:', unfollowers_list)
+    print(f'total count: {len(unfollowers_list)}')
+
+
+if __name__ == '__main__':
+    # TODO: sys 인자 값 미 입력시 프로그램 종료
+
+    ## Connect Driver
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.implicitly_wait(TIME_WAIT)
+
+    main(driver, sys.argv[1], sys.argv[2])
+
+    ## Close Driver
+    driver.close()
